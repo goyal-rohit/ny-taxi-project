69274b2300396c49bdfba9bfcf70d623a10be76f
Rohit Goyal <rohitgoyal.dds@gmail.com>
2024-05-05 20:30:59 +0530

    make dataset file complete, tested too

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -8,6 +8,29 @@ def load_data(data_path):
     df = pd.read_csv(data_path)
     return df
 
+def data_cleaning(df):
+    df.drop(columns='id', axis=1, inplace=True)
+
+    df["pickup_datetime"] = pd.to_datetime(df["pickup_datetime"])
+    df["pickup_hour"] = df["pickup_datetime"].dt.hour
+    df["pickup_minute"] = df["pickup_datetime"].dt.minute
+    df["pickup_second"] = df["pickup_datetime"].dt.second
+    df["pickup_minute_of_the_day"] = df["pickup_hour"] * 60 + df["pickup_minute"]
+    df["pickup_day_week"] =df["pickup_datetime"].dt.dayofweek
+    df["pickup_month"] = df["pickup_datetime"].dt.month
+
+
+    df["dropoff_datetime"] = pd.to_datetime(df["dropoff_datetime"])
+    df["dropoff_hour"] = df["dropoff_datetime"].dt.hour
+    df["dropoff_minute"] = df["dropoff_datetime"].dt.minute
+    df["dropoff_second"] = df["dropoff_datetime"].dt.second
+    df["dropoff_minute_of_the_day"] = df["dropoff_hour"] * 60 + df["dropoff_minute"]
+    df["dropoff_day_week"] =df["dropoff_datetime"].dt.dayofweek
+    df["dropoff_month"] = df["dropoff_datetime"].dt.month
+
+    df.drop(columns=["pickup_datetime", "dropoff_datetime"], axis=1, inplace=True)
+    return df
+
 def split_data(df, test_size, seed):
     train, test = train_test_split(df, test_size=test_size, random_state=seed)
     return train, test
@@ -28,7 +51,9 @@ def main():
     data_path = home_dir.as_posix() + input_file
     output_path = home_dir.as_posix() + '/data/processed'
 
-    data = load_data(data_path)
+    data1 = load_data(data_path)
+
+    data = data_cleaning(data1)
     train_data, test_data = split_data(data, params['test_size'], params['seed'])
     save_data(train_data, test_data, output_path)
 
