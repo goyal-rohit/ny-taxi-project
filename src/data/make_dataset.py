e38dadb0400dff915d323da4769b36126b457793
Rohit Goyal <rohitgoyal.dds@gmail.com>
2024-05-07 15:49:22 +0530

    build feature done

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,9 +4,11 @@ import sys
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+
 def load_data(data_path):
     df = pd.read_csv(data_path)
     return df
+
 
 def data_cleaning(df):
     df.drop(columns='id', axis=1, inplace=True)
@@ -16,32 +18,33 @@ def data_cleaning(df):
     df["pickup_minute"] = df["pickup_datetime"].dt.minute
     df["pickup_second"] = df["pickup_datetime"].dt.second
     df["pickup_minute_of_the_day"] = df["pickup_hour"] * 60 + df["pickup_minute"]
-    df["pickup_day_week"] =df["pickup_datetime"].dt.dayofweek
+    df["pickup_day_week"] = df["pickup_datetime"].dt.dayofweek
     df["pickup_month"] = df["pickup_datetime"].dt.month
-
 
     df["dropoff_datetime"] = pd.to_datetime(df["dropoff_datetime"])
     df["dropoff_hour"] = df["dropoff_datetime"].dt.hour
     df["dropoff_minute"] = df["dropoff_datetime"].dt.minute
     df["dropoff_second"] = df["dropoff_datetime"].dt.second
     df["dropoff_minute_of_the_day"] = df["dropoff_hour"] * 60 + df["dropoff_minute"]
-    df["dropoff_day_week"] =df["dropoff_datetime"].dt.dayofweek
+    df["dropoff_day_week"] = df["dropoff_datetime"].dt.dayofweek
     df["dropoff_month"] = df["dropoff_datetime"].dt.month
 
     df.drop(columns=["pickup_datetime", "dropoff_datetime"], axis=1, inplace=True)
     return df
 
+
 def split_data(df, test_size, seed):
     train, test = train_test_split(df, test_size=test_size, random_state=seed)
     return train, test
 
+
 def save_data(train, test, out_path):
     pathlib.Path(out_path).mkdir(parents=True, exist_ok=True)
-    train.to_csv(out_path+'/train.csv', index=False)
-    test.to_csv(out_path+'/test.csv', index=False)
+    train.to_csv(out_path + '/train.csv', index=False)
+    test.to_csv(out_path + '/test.csv', index=False)
+
 
 def main():
-    
     curr_dir = pathlib.Path(__file__)
     home_dir = curr_dir.parent.parent.parent
     params_file = home_dir.as_posix() + '/params.yaml'
@@ -56,6 +59,7 @@ def main():
     data = data_cleaning(data1)
     train_data, test_data = split_data(data, params['test_size'], params['seed'])
     save_data(train_data, test_data, output_path)
+
 
 if __name__ == "__main__":
     main()
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,13 +1,46 @@
-import pathlib
-import yaml
-import sys
 import pandas as pd
+import numpy as np
+import sys, pathlib
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+import pickle
 
 
+def transform_obj(train_df):
+    cat_columns = train_df.columns[train_df.dtypes == 'object']
+    num_columns = train_df.columns[(train_df.dtypes == 'float') | (train_df.dtypes == 'int')]
+
+    preprocessor = ColumnTransformer(transformers=[
+        ('ohe', OneHotEncoder(drop='first', dtype=np.int32), cat_columns),
+        ('ss', StandardScaler(), num_columns)
+    ], remainder='passthrough'
+    )
+    return preprocessor
+
+
+def transform_data(train_path, test_path):
+    train_df = pd.read_csv(train_path)
+    test_df = pd.read_csv(test_path)
+    transformed_obj = transform_obj(train_df)
+
+    target_col_name = ['trip_duration']
+
+    input_features_train_df = train_df.drop(target_col_name, axis=1)
+    target_feature_train_df = train_df[target_col_name]
+
+    input_features_test_df = test_df.drop(target_col_name, axis=1)
+    target_feature_test_df = test_df[target_col_name]
+
+    input_feature_train_arr = transformed_obj.fit_transform(input_features_train_df)
+    input_feature_test_arr = transformed_obj.transform(input_features_test_df)
+
+    train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+    test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+    return train_arr, test_arr, transformed_obj
 
 
 def main():
-    
     curr_dir = pathlib.Path(__file__)
     home_dir = curr_dir.parent.parent.parent
 
@@ -18,7 +51,16 @@ def main():
 
     output_path = home_dir.as_posix() + '/data/processed'
 
- 
+    train_arr, test_arr, transformed_obj = transform_data(train_path, test_path)
+
+    pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
+    with open(output_path+'/trans_obj.pkl','wb') as f:
+        pickle.dump(transformed_obj,f)
+
+    #save_obj(output_path, 'trans_obj.pkl', transformed_obj)
+    np.savez(output_path + '/train_arr', train_arr)
+    np.savez(output_path + '/test_arr', test_arr)
+
 
 if __name__ == "__main__":
     main()
